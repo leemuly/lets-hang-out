f4b9fae8f98b24bde67b6bf412162caa204e66c7
Marigrace Leeman <leemuly@gmail.com>
2019-08-09 12:19:43 -0400

    firebase initialized

---
+++ firebase/firebase.js
@@ -0,0 +1,30 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+export class FirebaseWrapper {
+  constructor() {
+    this.initialized = false;
+    this._firebaseInstance = null;
+    this._firebaseWrapperInstance = null;
+    this._firestore = null;
+  }
+
+  Initialize(config) {
+    if (!this.initialized) {
+      this._firebaseInstance = firebase.initializeApp(config);
+      this._firestore = firebase.firestore();
+      this.initialized = true;
+      console.log('firebase initialized :)')
+    } else {
+      console.log('already initialized!');
+    }
+  }
+
+  static GetInstance() {
+    if (null == this._firebaseWrapperInstance) {
+      this._firebaseWrapperInstance = new FirebaseWrapper();
+    }
+
+    return this._firebaseWrapperInstance;
+  }
+}
